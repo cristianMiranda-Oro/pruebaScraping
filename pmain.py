ba02b5e495db07cbef41a403eed43bda0cc6a528
cristianMiranda-Oro <cristianmirandaororstegui@gmail.com>
2022-01-09 14:05:48 -0500

    initial config

---
+++ pmain.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 13:54:35 2022
+
+@author: CRISTIANMIRANDA
+"""
+
+
+import os, random, sys, E:\Documentos\udemy\linkedin\.gitignore
+from urllib.parse import urlparse
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
